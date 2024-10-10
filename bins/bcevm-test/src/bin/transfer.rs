4bc7ac127f11daeffdb9954a51b6500921eb47f5
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-10 23:10:45 +0300

    Update transfer.rs

--- bins/bcevm-test/src/bin/transfer.rs
+++ bins/bcevm-test/src/bin/transfer.rs
@@ -3,30 +3,19 @@ use bcevm::{
     primitives::{Bytecode, TransactTo, U256},
     Evm,
 };
-
 use std::time::Duration;
 
 fn main() {
-    // BenchmarkDB is dummy state that implements Database trait.
     let mut evm = Evm::builder()
         .with_db(BenchmarkDB::new_bytecode(Bytecode::new()))
         .modify_tx_env(|tx| {
-            // execution globals block hash/gas_limit/coinbase/timestamp..
-            tx.caller = "0x0000000000000000000000000000000000000001"
-                .parse()
-                .unwrap();
+            tx.caller = "0x0000000000000000000000000000000000000001".parse().unwrap();
             tx.value = U256::from(10);
-            tx.transact_to = TransactTo::Call(
-                "0x0000000000000000000000000000000000000000"
-                    .parse()
-                    .unwrap(),
-            );
+            tx.transact_to = TransactTo::Call("0x0000000000000000000000000000000000000000".parse().unwrap());
         })
         .build();
 
-    // Microbenchmark
     let bench_options = microbench::Options::default().time(Duration::from_secs(3));
-
     microbench::bench(&bench_options, "Simple value transfer", || {
         let _ = evm.transact().unwrap();
     });
