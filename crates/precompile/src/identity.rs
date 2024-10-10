932c373eb5a7bdfe1393838b4125abe27abf33e1
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-10 22:57:19 +0300

    Update identity.rs

--- crates/precompile/src/identity.rs
+++ crates/precompile/src/identity.rs
@@ -2,22 +2,15 @@ use super::calc_linear_cost_u32;
 use crate::{Error, Precompile, PrecompileResult, PrecompileWithAddress};
 use bcevm_primitives::Bytes;
 
-pub const FUN: PrecompileWithAddress =
-    PrecompileWithAddress(crate::u64_to_address(4), Precompile::Standard(identity_run));
-
-/// The base cost of the operation.
+pub const FUN: PrecompileWithAddress = PrecompileWithAddress(crate::u64_to_address(4), Precompile::Standard(identity_run));
 pub const IDENTITY_BASE: u64 = 15;
-/// The cost per word.
 pub const IDENTITY_PER_WORD: u64 = 3;
 
-/// Takes the input bytes, copies them, and returns it as the output.
-///
-/// See: <https://ethereum.github.io/yellowpaper/paper.pdf>
-/// See: <https://etherscan.io/address/0000000000000000000000000000000000000004>
 pub fn identity_run(input: &Bytes, gas_limit: u64) -> PrecompileResult {
     let gas_used = calc_linear_cost_u32(input.len(), IDENTITY_BASE, IDENTITY_PER_WORD);
     if gas_used > gas_limit {
-        return Err(Error::OutOfGas);
+        Err(Error::OutOfGas)
+    } else {
+        Ok((gas_used, input.clone()))
     }
-    Ok((gas_used, input.clone()))
 }
