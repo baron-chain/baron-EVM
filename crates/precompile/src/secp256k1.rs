2415afcd23dd31ca0eef42921810576f9e95e9dd
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-10 23:03:00 +0300

    Update secp256k1.rs

--- crates/precompile/src/secp256k1.rs
+++ crates/precompile/src/secp256k1.rs
@@ -9,57 +9,36 @@ pub const ECRECOVER: PrecompileWithAddress = PrecompileWithAddress(
 pub use self::secp256k1::ecrecover;
 
 #[cfg(not(feature = "secp256k1"))]
-#[allow(clippy::module_inception)]
 mod secp256k1 {
     use k256::ecdsa::{Error, RecoveryId, Signature, VerifyingKey};
     use bcevm_primitives::{alloy_primitives::B512, keccak256, B256};
 
     pub fn ecrecover(sig: &B512, mut recid: u8, msg: &B256) -> Result<B256, Error> {
-        // parse signature
         let mut sig = Signature::from_slice(sig.as_slice())?;
-
-        // normalize signature and flip recovery id if needed.
         if let Some(sig_normalized) = sig.normalize_s() {
             sig = sig_normalized;
             recid ^= 1;
         }
         let recid = RecoveryId::from_byte(recid).expect("recovery ID is valid");
-
-        // recover key
         let recovered_key = VerifyingKey::recover_from_prehash(&msg[..], &sig, recid)?;
-        // hash it
-        let mut hash = keccak256(
-            &recovered_key
-                .to_encoded_point(/* compress = */ false)
-                .as_bytes()[1..],
-        );
-
-        // truncate to 20 bytes
+        let mut hash = keccak256(&recovered_key.to_encoded_point(false).as_bytes()[1..]);
         hash[..12].fill(0);
         Ok(hash)
     }
 }
 
 #[cfg(feature = "secp256k1")]
-#[allow(clippy::module_inception)]
 mod secp256k1 {
     use bcevm_primitives::{alloy_primitives::B512, keccak256, B256};
-    use secp256k1::{
-        ecdsa::{RecoverableSignature, RecoveryId},
-        Message, Secp256k1,
-    };
-
-    // Silence the unused crate dependency warning.
+    use secp256k1::{ecdsa::{RecoverableSignature, RecoveryId}, Message, Secp256k1};
     use k256 as _;
 
     pub fn ecrecover(sig: &B512, recid: u8, msg: &B256) -> Result<B256, secp256k1::Error> {
         let recid = RecoveryId::from_i32(recid as i32).expect("recovery ID is valid");
         let sig = RecoverableSignature::from_compact(sig.as_slice(), recid)?;
-
         let secp = Secp256k1::new();
         let msg = Message::from_digest(msg.0);
         let public = secp.recover_ecdsa(&msg, &sig)?;
-
         let mut hash = keccak256(&public.serialize_uncompressed()[1..]);
         hash[..12].fill(0);
         Ok(hash)
@@ -68,22 +47,16 @@ mod secp256k1 {
 
 pub fn ec_recover_run(input: &Bytes, gas_limit: u64) -> PrecompileResult {
     const ECRECOVER_BASE: u64 = 3_000;
-
     if ECRECOVER_BASE > gas_limit {
         return Err(Error::OutOfGas);
     }
-
     let input = right_pad::<128>(input);
-
-    // `v` must be a 32-byte big-endian integer equal to 27 or 28.
     if !(input[32..63].iter().all(|&b| b == 0) && matches!(input[63], 27 | 28)) {
         return Ok((ECRECOVER_BASE, Bytes::new()));
     }
-
     let msg = <&B256>::try_from(&input[0..32]).unwrap();
     let recid = input[63] - 27;
     let sig = <&B512>::try_from(&input[64..128]).unwrap();
-
     let out = secp256k1::ecrecover(sig, recid, msg)
         .map(|o| o.to_vec().into())
         .unwrap_or_default();
