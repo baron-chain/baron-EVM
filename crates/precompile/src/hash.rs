2505ea6971a6d70d4d368a3e51be37a831ad027d
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-10 22:56:07 +0300

    Update hash.rs

--- crates/precompile/src/hash.rs
+++ crates/precompile/src/hash.rs
@@ -3,40 +3,25 @@ use crate::{Error, Precompile, PrecompileResult, PrecompileWithAddress};
 use bcevm_primitives::Bytes;
 use sha2::Digest;
 
-pub const SHA256: PrecompileWithAddress =
-    PrecompileWithAddress(crate::u64_to_address(2), Precompile::Standard(sha256_run));
+pub const SHA256: PrecompileWithAddress = PrecompileWithAddress(crate::u64_to_address(2), Precompile::Standard(sha256_run));
+pub const RIPEMD160: PrecompileWithAddress = PrecompileWithAddress(crate::u64_to_address(3), Precompile::Standard(ripemd160_run));
 
-pub const RIPEMD160: PrecompileWithAddress = PrecompileWithAddress(
-    crate::u64_to_address(3),
-    Precompile::Standard(ripemd160_run),
-);
-
-/// See: <https://ethereum.github.io/yellowpaper/paper.pdf>
-/// See: <https://docs.soliditylang.org/en/develop/units-and-global-variables.html#mathematical-and-cryptographic-functions>
-/// See: <https://etherscan.io/address/0000000000000000000000000000000000000002>
 pub fn sha256_run(input: &Bytes, gas_limit: u64) -> PrecompileResult {
     let cost = calc_linear_cost_u32(input.len(), 60, 12);
     if cost > gas_limit {
         Err(Error::OutOfGas)
     } else {
-        let output = sha2::Sha256::digest(input);
-        Ok((cost, output.to_vec().into()))
+        Ok((cost, sha2::Sha256::digest(input).to_vec().into()))
     }
 }
 
-/// See: <https://ethereum.github.io/yellowpaper/paper.pdf>
-/// See: <https://docs.soliditylang.org/en/develop/units-and-global-variables.html#mathematical-and-cryptographic-functions>
-/// See: <https://etherscan.io/address/0000000000000000000000000000000000000003>
 pub fn ripemd160_run(input: &Bytes, gas_limit: u64) -> PrecompileResult {
     let gas_used = calc_linear_cost_u32(input.len(), 600, 120);
     if gas_used > gas_limit {
         Err(Error::OutOfGas)
     } else {
-        let mut hasher = ripemd::Ripemd160::new();
-        hasher.update(input);
-
         let mut output = [0u8; 32];
-        hasher.finalize_into((&mut output[12..]).into());
+        ripemd::Ripemd160::digest(input).copy_to_slice(&mut output[12..]);
         Ok((gas_used, output.to_vec().into()))
     }
 }
