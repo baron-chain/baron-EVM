18e01ad9f14c8f3001905d111dd5daa44318c0b9
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-11 23:59:17 +0300

    Update evmrunner.rs

--- bins/bcevme/src/cmd/evmrunner.rs
+++ bins/bcevme/src/cmd/evmrunner.rs
@@ -1,15 +1,6 @@
-use bcevm::{
-    db::BenchmarkDB,
-    primitives::{Address, Bytecode, TransactTo},
-    Evm,
-};
-use std::io::Error as IoError;
-use std::path::PathBuf;
-use std::time::Duration;
-use std::{borrow::Cow, fs};
+use bcevm::{db::BenchmarkDB, primitives::{Address, Bytecode, TransactTo}, Evm};
+use std::{io::Error as IoError, path::PathBuf, time::Duration, borrow::Cow, fs};
 use structopt::StructOpt;
-
-extern crate alloc;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Errors {
@@ -22,75 +13,47 @@ pub enum Errors {
     #[error("EVM Error")]
     EVMError,
     #[error(transparent)]
-    Io(IoError),
+    Io(#[from] IoError),
 }
 
-impl From<IoError> for Errors {
-    fn from(e: IoError) -> Self {
-        Errors::Io(e)
-    }
-}
-
-/// Evm runner command allows running arbitrary evm bytecode.
-/// Bytecode can be provided from cli or from file with --path option.
 #[derive(StructOpt, Debug)]
 pub struct Cmd {
-    /// Bytecode to be executed.
     #[structopt(default_value = "")]
     bytecode: String,
-    /// Path to file containing the evm bytecode.
-    /// Overrides the bytecode option.
     #[structopt(long)]
     path: Option<PathBuf>,
-    /// Run in benchmarking mode.
     #[structopt(long)]
     bench: bool,
-    /// Input bytes.
     #[structopt(long, default_value = "")]
     input: String,
-    /// Print the state.
     #[structopt(long)]
     state: bool,
 }
 
 impl Cmd {
-    /// Run statetest command.
     pub fn run(&self) -> Result<(), Errors> {
         let bytecode_str: Cow<'_, str> = if let Some(path) = &self.path {
-            // check if path exists.
             if !path.exists() {
                 return Err(Errors::PathNotExists);
             }
-            fs::read_to_string(path)?.to_owned().into()
+            fs::read_to_string(path)?.into()
         } else {
             self.bytecode.as_str().into()
         };
-
         let bytecode = hex::decode(bytecode_str.trim()).map_err(|_| Errors::InvalidBytecode)?;
-        let input = hex::decode(self.input.trim())
-            .map_err(|_| Errors::InvalidInput)?
-            .into();
-        // BenchmarkDB is dummy state that implements Database trait.
-        // the bytecode is deployed at zero address.
+        let input = hex::decode(self.input.trim()).map_err(|_| Errors::InvalidInput)?;
+
         let mut evm = Evm::builder()
-            .with_db(BenchmarkDB::new_bytecode(Bytecode::new_raw(
-                bytecode.into(),
-            )))
+            .with_db(BenchmarkDB::new_bytecode(Bytecode::new_raw(bytecode.into())))
             .modify_tx_env(|tx| {
-                // execution globals block hash/gas_limit/coinbase/timestamp..
-                tx.caller = "0x0000000000000000000000000000000000000001"
-                    .parse()
-                    .unwrap();
+                tx.caller = "0x0000000000000000000000000000000000000001".parse().unwrap();
                 tx.transact_to = TransactTo::Call(Address::ZERO);
-                tx.data = input;
+                tx.data = input.into();
             })
             .build();
 
         if self.bench {
-            // Microbenchmark
-            let bench_options = microbench::Options::default().time(Duration::from_secs(3));
-
-            microbench::bench(&bench_options, "Run bytecode", || {
+            microbench::bench(&microbench::Options::default().time(Duration::from_secs(3)), "Run bytecode", || {
                 let _ = evm.transact().unwrap();
             });
         } else {
