cc330ff8c9336d62c9aaf5511ce490f28a6bd9fd
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-12 00:06:09 +0300

    Update mod.rs

--- bins/bcevme/src/cmd/statetest/models/mod.rs
+++ bins/bcevme/src/cmd/statetest/models/mod.rs
@@ -14,10 +14,8 @@ pub struct TestSuite(pub BTreeMap<String, TestUnit>);
 #[derive(Debug, PartialEq, Eq, Deserialize)]
 #[serde(deny_unknown_fields)]
 pub struct TestUnit {
-    /// Test info is optional
     #[serde(default, rename = "_info")]
     pub info: Option<serde_json::Value>,
-
     pub env: Env,
     pub pre: HashMap<Address, AccountInfo>,
     pub post: BTreeMap<SpecName, Vec<Test>>,
@@ -26,24 +24,15 @@ pub struct TestUnit {
     pub out: Option<Bytes>,
 }
 
-/// State test indexed state result deserialization.
 #[derive(Debug, PartialEq, Eq, Deserialize)]
 #[serde(rename_all = "camelCase", deny_unknown_fields)]
 pub struct Test {
     pub expect_exception: Option<String>,
-
-    /// Indexes
     pub indexes: TxPartIndices,
-    /// Post state hash
     pub hash: B256,
-    /// Post state
     #[serde(default)]
     pub post_state: HashMap<Address, AccountInfo>,
-
-    /// Logs root
     pub logs: B256,
-
-    /// Tx bytes
     pub txbytes: Option<Bytes>,
 }
 
@@ -75,11 +64,9 @@ pub struct Env {
     pub current_timestamp: U256,
     pub current_base_fee: Option<U256>,
     pub previous_hash: Option<B256>,
-
     pub current_random: Option<B256>,
     pub current_beacon_root: Option<B256>,
     pub current_withdrawals_root: Option<B256>,
-
     pub parent_blob_gas_used: Option<U256>,
     pub parent_excess_blob_gas: Option<U256>,
     pub current_excess_blob_gas: Option<U256>,
@@ -93,7 +80,6 @@ pub struct TransactionParts {
     pub gas_price: Option<U256>,
     pub nonce: U256,
     pub secret_key: B256,
-    /// if sender is not present we need to derive it from secret key.
     #[serde(default)]
     pub sender: Option<Address>,
     #[serde(deserialize_with = "deserialize_maybe_empty")]
@@ -101,10 +87,8 @@ pub struct TransactionParts {
     pub value: Vec<U256>,
     pub max_fee_per_gas: Option<U256>,
     pub max_priority_fee_per_gas: Option<U256>,
-
     #[serde(default)]
     pub access_lists: Vec<Option<AccessList>>,
-
     #[serde(default)]
     pub blob_versioned_hashes: Vec<B256>,
     pub max_fee_per_blob_gas: Option<U256>,
@@ -121,41 +105,31 @@ pub type AccessList = Vec<AccessListItem>;
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
     use serde_json::Error;
 
     #[test]
-    pub fn serialize_u256() -> Result<(), Error> {
+    fn serialize_u256() -> Result<(), Error> {
         let json = r#"{"_item":"0x10"}"#;
-
         #[derive(Deserialize, Debug)]
-        pub struct Test {
-            _item: Option<U256>,
-        }
-
+        struct Test { _item: Option<U256> }
         let out: Test = serde_json::from_str(json)?;
         println!("out:{out:?}");
         Ok(())
     }
 
     #[test]
-    pub fn deserialize_minimal_transaction_parts() -> Result<(), Error> {
+    fn deserialize_minimal_transaction_parts() -> Result<(), Error> {
         let json = r#"{"data":[],"gasLimit":[],"nonce":"0x0","secretKey":"0x0000000000000000000000000000000000000000000000000000000000000000","to":"","value":[]}"#;
-
         let _: TransactionParts = serde_json::from_str(json)?;
         Ok(())
     }
 
     #[test]
-    pub fn serialize_b160() -> Result<(), Error> {
+    fn serialize_b160() -> Result<(), Error> {
         let json = r#"{"_item":"0x2adc25665018aa1fe0e6bc666dac8fc2697ff9ba"}"#;
-
         #[derive(Deserialize, Debug)]
-        pub struct Test {
-            _item: Address,
-        }
-
+        struct Test { _item: Address }
         let out: Test = serde_json::from_str(json)?;
         println!("out:{out:?}");
         Ok(())
