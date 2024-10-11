24aa7667320324dd9f45de3ebb12d7722271a58b
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-12 00:00:22 +0300

    Update bytecode.rs

--- bins/bcevme/src/cmd/bytecode.rs
+++ bins/bcevme/src/cmd/bytecode.rs
@@ -1,37 +1,27 @@
-use bcevm::{
-    interpreter::opcode::eof_printer::print_eof_code,
-    primitives::{Bytes, Eof},
-};
+use bcevm::{interpreter::opcode::eof_printer::print_eof_code, primitives::{Bytes, Eof}};
 use structopt::StructOpt;
 
-/// Statetest command
 #[derive(StructOpt, Debug)]
 pub struct Cmd {
-    /// EOF bytecode in hex format. It bytes start with 0xFE it will be interpreted as a EOF.
-    /// Otherwise, it will be interpreted as a EOF bytecode.
     #[structopt(required = true)]
     bytes: String,
 }
 
 impl Cmd {
-    /// Run statetest command.
     pub fn run(&self) {
-        let trimmed = self.bytes.trim_start_matches("0x");
-        let Ok(bytes) = hex::decode(trimmed) else {
-            eprintln!("Invalid hex string");
-            return;
-        };
-        let bytes: Bytes = bytes.into();
-        if bytes.is_empty() {
-            eprintln!("Empty hex string");
-            return;
-        }
-        if bytes[0] == 0xEF {
-            let Ok(eof) = Eof::decode(bytes) else {
-                eprintln!("Invalid EOF bytecode");
+        let bytes: Bytes = match hex::decode(self.bytes.trim_start_matches("0x")) {
+            Ok(b) if !b.is_empty() => b.into(),
+            _ => {
+                eprintln!("Invalid or empty hex string");
                 return;
-            };
-            println!("{:#?}", eof);
+            }
+        };
+
+        if bytes[0] == 0xEF {
+            match Eof::decode(bytes) {
+                Ok(eof) => println!("{:#?}", eof),
+                Err(_) => eprintln!("Invalid EOF bytecode"),
+            }
         } else {
             print_eof_code(&bytes)
         }
