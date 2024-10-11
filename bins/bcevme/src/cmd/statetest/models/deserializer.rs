28f4705c909b81734d6634bf067dd6e00521b0f3
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-12 00:07:06 +0300

    Update deserializer.rs

--- bins/bcevme/src/cmd/statetest/models/deserializer.rs
+++ bins/bcevme/src/cmd/statetest/models/deserializer.rs
@@ -5,24 +5,23 @@ pub fn deserialize_str_as_u64<'de, D>(deserializer: D) -> Result<u64, D::Error>
 where
     D: de::Deserializer<'de>,
 {
-    let string = String::deserialize(deserializer)?;
-
-    if let Some(stripped) = string.strip_prefix("0x") {
-        u64::from_str_radix(stripped, 16)
-    } else {
-        string.parse()
-    }
-    .map_err(serde::de::Error::custom)
+    String::deserialize(deserializer)?
+        .strip_prefix("0x")
+        .map_or_else(
+            |s| s.parse(),
+            |s| u64::from_str_radix(s, 16)
+        )
+        .map_err(de::Error::custom)
 }
 
 pub fn deserialize_maybe_empty<'de, D>(deserializer: D) -> Result<Option<Address>, D::Error>
 where
     D: de::Deserializer<'de>,
 {
-    let string = String::deserialize(deserializer)?;
-    if string.is_empty() {
+    let s = String::deserialize(deserializer)?;
+    if s.is_empty() {
         Ok(None)
     } else {
-        string.parse().map_err(de::Error::custom).map(Some)
+        s.parse().map(Some).map_err(de::Error::custom)
     }
 }
