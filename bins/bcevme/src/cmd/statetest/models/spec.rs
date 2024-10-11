ffe8107ace750b0d42a14e2be22cec9e7d6c6164
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-12 00:04:53 +0300

    Update spec.rs

--- bins/bcevme/src/cmd/statetest/models/spec.rs
+++ bins/bcevme/src/cmd/statetest/models/spec.rs
@@ -9,12 +9,12 @@ pub enum SpecName {
     HomesteadToDaoAt5,
     HomesteadToEIP150At5,
     EIP150,
-    EIP158, // EIP-161: State trie clearing
+    EIP158,
     EIP158ToByzantiumAt5,
     Byzantium,
-    ByzantiumToConstantinopleAt5, // SKIPPED
+    ByzantiumToConstantinopleAt5,
     ByzantiumToConstantinopleFixAt5,
-    Constantinople, // SKIPPED
+    Constantinople,
     ConstantinopleFix,
     Istanbul,
     Berlin,
@@ -32,9 +32,7 @@ impl SpecName {
         match self {
             Self::Frontier => SpecId::FRONTIER,
             Self::Homestead | Self::FrontierToHomesteadAt5 => SpecId::HOMESTEAD,
-            Self::EIP150 | Self::HomesteadToDaoAt5 | Self::HomesteadToEIP150At5 => {
-                SpecId::TANGERINE
-            }
+            Self::EIP150 | Self::HomesteadToDaoAt5 | Self::HomesteadToEIP150At5 => SpecId::TANGERINE,
             Self::EIP158 => SpecId::SPURIOUS_DRAGON,
             Self::Byzantium | Self::EIP158ToByzantiumAt5 => SpecId::BYZANTIUM,
             Self::ConstantinopleFix | Self::ByzantiumToConstantinopleFixAt5 => SpecId::PETERSBURG,
@@ -44,9 +42,7 @@ impl SpecName {
             Self::Merge => SpecId::MERGE,
             Self::Shanghai => SpecId::SHANGHAI,
             Self::Cancun => SpecId::CANCUN,
-            Self::ByzantiumToConstantinopleAt5 | Self::Constantinople => {
-                panic!("Overridden with PETERSBURG")
-            }
+            Self::ByzantiumToConstantinopleAt5 | Self::Constantinople => panic!("Overridden with PETERSBURG"),
             Self::Unknown => panic!("Unknown spec"),
         }
     }
