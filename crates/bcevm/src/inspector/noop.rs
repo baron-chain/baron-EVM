79b94314e5e09e74fb780e9afc0d11eff6e793cf
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-16 23:23:02 +0300

    Update noop.rs

--- crates/bcevm/src/inspector/noop.rs
+++ crates/bcevm/src/inspector/noop.rs
@@ -1,5 +1,6 @@
 use crate::{Database, Inspector};
-/// Dummy [Inspector], helpful as standalone replacement.
+
+/// Dummy [Inspector] that performs no operations.
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct NoOpInspector;
 
