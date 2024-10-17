8c0ca0090ef686254851e2a814a29bf94486d6f8
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-17 22:50:35 +0300

    Update handle_types.rs

--- crates/bcevm/src/handler/handle_types.rs
+++ crates/bcevm/src/handler/handle_types.rs
@@ -1,25 +1,36 @@
-// Modules
-
 pub mod execution;
 pub mod post_execution;
 pub mod pre_execution;
 pub mod validation;
 
-// Exports
-
 pub use validation::{
-    ValidateEnvHandle, ValidateInitialTxGasHandle, ValidateTxEnvAgainstState, ValidationHandler,
+    ValidateEnvHandle,
+    ValidateInitialTxGasHandle,
+    ValidateTxEnvAgainstState,
+    ValidationHandler,
 };
 
 pub use execution::{
-    ExecutionHandler, FrameCallHandle, FrameCallReturnHandle, FrameCreateHandle,
-    FrameCreateReturnHandle, InsertCallOutcomeHandle, InsertCreateOutcomeHandle,
+    ExecutionHandler,
+    FrameCallHandle,
+    FrameCallReturnHandle,
+    FrameCreateHandle,
+    FrameCreateReturnHandle,
+    InsertCallOutcomeHandle,
+    InsertCreateOutcomeHandle,
 };
 
 pub use pre_execution::{
-    DeductCallerHandle, LoadAccountsHandle, LoadPrecompilesHandle, PreExecutionHandler,
+    DeductCallerHandle,
+    LoadAccountsHandle,
+    LoadPrecompilesHandle,
+    PreExecutionHandler,
 };
 
 pub use post_execution::{
-    EndHandle, OutputHandle, PostExecutionHandler, ReimburseCallerHandle, RewardBeneficiaryHandle,
+    EndHandle,
+    OutputHandle,
+    PostExecutionHandler,
+    ReimburseCallerHandle,
+    RewardBeneficiaryHandle,
 };
