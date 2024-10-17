0bb00a4a0b4e2bacc77fecb988a51c39df35a8bb
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-17 22:45:26 +0300

    Update eip3155.rs

--- crates/bcevm/src/inspector/eip3155.rs
+++ crates/bcevm/src/inspector/eip3155.rs
@@ -1,8 +1,6 @@
 use crate::{
     inspectors::GasInspector,
-    interpreter::{
-        CallInputs, CallOutcome, CreateInputs, CreateOutcome, Interpreter, InterpreterResult,
-    },
+    interpreter::{CallInputs, CallOutcome, CreateInputs, CreateOutcome, Interpreter, InterpreterResult},
     primitives::{db::Database, hex, HashMap, B256, U256},
     EvmContext, Inspector,
 };
@@ -10,14 +8,47 @@ use bcevm_interpreter::OpCode;
 use serde::Serialize;
 use std::io::Write;
 
-/// [EIP-3155](https://eips.ethereum.org/EIPS/eip-3155) tracer [Inspector].
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct Output {
+    pc: u64,
+    op: u8,
+    gas: String,
+    gas_cost: String,
+    stack: Vec<String>,
+    depth: u64,
+    return_data: String,
+    refund: String,
+    mem_size: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    op_name: Option<&'static str>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    error: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    memory: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    storage: Option<HashMap<String, String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    return_stack: Option<Vec<String>>,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct Summary {
+    state_root: String,
+    output: String,
+    gas_used: String,
+    pass: bool,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    time: Option<u128>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    fork: Option<String>,
+}
+
 pub struct TracerEip3155 {
     output: Box<dyn Write>,
     gas_inspector: GasInspector,
-
-    /// Print summary of the execution.
     print_summary: bool,
-
     stack: Vec<U256>,
     pc: usize,
     opcode: u8,
@@ -27,103 +58,6 @@ pub struct TracerEip3155 {
     skip: bool,
     include_memory: bool,
     memory: Option<String>,
-}
-
-// # Output
-// The CUT MUST output a `json` object for EACH operation.
-#[derive(Serialize)]
-#[serde(rename_all = "camelCase")]
-struct Output {
-    // Required fields:
-    /// Program counter
-    pc: u64,
-    /// OpCode
-    op: u8,
-    /// Gas left before executing this operation
-    gas: String,
-    /// Gas cost of this operation
-    gas_cost: String,
-    /// Array of all values on the stack
-    stack: Vec<String>,
-    /// Depth of the call stack
-    depth: u64,
-    /// Data returned by the function call
-    return_data: String,
-    /// Amount of **global** gas refunded
-    refund: String,
-    /// Size of memory array
-    mem_size: String,
-
-    // Optional fields:
-    /// Name of the operation
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    op_name: Option<&'static str>,
-    /// Description of an error (should contain revert reason if supported)
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    error: Option<String>,
-    /// Array of all allocated values
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    memory: Option<String>,
-    /// Array of all stored values
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    storage: Option<HashMap<String, String>>,
-    /// Array of values, Stack of the called function
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    return_stack: Option<Vec<String>>,
-}
-
-// # Summary and error handling
-#[derive(Serialize)]
-#[serde(rename_all = "camelCase")]
-struct Summary {
-    // Required fields:
-    /// Root of the state trie after executing the transaction
-    state_root: String,
-    /// Return values of the function
-    output: String,
-    /// All gas used by the transaction
-    gas_used: String,
-    /// Bool whether transaction was executed successfully
-    pass: bool,
-
-    // Optional fields:
-    /// Time in nanoseconds needed to execute the transaction
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    time: Option<u128>,
-    /// Name of the fork rules used for execution
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    fork: Option<String>,
-}
-
-impl TracerEip3155 {
-    /// Sets the writer to use for the output.
-    pub fn set_writer(&mut self, writer: Box<dyn Write>) {
-        self.output = writer;
-    }
-
-    /// Resets the Tracer to its initial state of [Self::new].
-    /// This makes the inspector ready to be used again.
-    pub fn clear(&mut self) {
-        let Self {
-            gas_inspector,
-            stack,
-            pc,
-            opcode,
-            gas,
-            refunded,
-            mem_size,
-            skip,
-            ..
-        } = self;
-        *gas_inspector = GasInspector::default();
-        stack.clear();
-        *pc = 0;
-        *opcode = 0;
-        *gas = 0;
-        *refunded = 0;
-        *mem_size = 0;
-        *skip = false;
-    }
 }
 
 impl TracerEip3155 {
@@ -144,13 +78,26 @@ impl TracerEip3155 {
         }
     }
 
-    /// Don't include a summary at the end of the trace
+    pub fn set_writer(&mut self, writer: Box<dyn Write>) {
+        self.output = writer;
+    }
+
+    pub fn clear(&mut self) {
+        self.gas_inspector = GasInspector::default();
+        self.stack.clear();
+        self.pc = 0;
+        self.opcode = 0;
+        self.gas = 0;
+        self.refunded = 0;
+        self.mem_size = 0;
+        self.skip = false;
+    }
+
     pub fn without_summary(mut self) -> Self {
         self.print_summary = false;
         self
     }
 
-    /// Include a memory field for each step. This significantly increases processing time and output size.
     pub fn with_memory(mut self) -> Self {
         self.include_memory = true;
         self
@@ -162,19 +109,13 @@ impl TracerEip3155 {
         self.output.flush()
     }
 
-    fn print_summary<DB: Database>(
-        &mut self,
-        result: &InterpreterResult,
-        context: &mut EvmContext<DB>,
-    ) {
+    fn print_summary<DB: Database>(&mut self, result: &InterpreterResult, context: &mut EvmContext<DB>) {
         if self.print_summary {
             let spec_name: &str = context.spec_id().into();
             let value = Summary {
                 state_root: B256::ZERO.to_string(),
                 output: result.output.to_string(),
-                gas_used: hex_number(
-                    context.inner.env().tx.gas_limit - self.gas_inspector.gas_remaining(),
-                ),
+                gas_used: hex_number(context.inner.env().tx.gas_limit - self.gas_inspector.gas_remaining()),
                 pass: result.is_ok(),
                 time: None,
                 fork: Some(spec_name.to_string()),
@@ -192,11 +133,7 @@ impl<DB: Database> Inspector<DB> for TracerEip3155 {
     fn step(&mut self, interp: &mut Interpreter, context: &mut EvmContext<DB>) {
         self.gas_inspector.step(interp, context);
         self.stack = interp.stack.data().clone();
-        self.memory = if self.include_memory {
-            Some(hex::encode_prefixed(interp.shared_memory.context_memory()))
-        } else {
-            None
-        };
+        self.memory = self.include_memory.then(|| hex::encode_prefixed(interp.shared_memory.context_memory()));
         self.pc = interp.program_counter();
         self.opcode = interp.current_opcode();
         self.mem_size = interp.shared_memory.len();
@@ -221,13 +158,8 @@ impl<DB: Database> Inspector<DB> for TracerEip3155 {
             return_data: "0x".to_string(),
             refund: hex_number(self.refunded as u64),
             mem_size: self.mem_size.to_string(),
-
             op_name: OpCode::new(self.opcode).map(|i| i.as_str()),
-            error: if !interp.instruction_result.is_ok() {
-                Some(format!("{:?}", interp.instruction_result))
-            } else {
-                None
-            },
+            error: (!interp.instruction_result.is_ok()).then(|| format!("{:?}", interp.instruction_result)),
             memory: self.memory.take(),
             storage: None,
             return_stack: None,
@@ -235,38 +167,21 @@ impl<DB: Database> Inspector<DB> for TracerEip3155 {
         let _ = self.write_value(&value);
     }
 
-    fn call_end(
-        &mut self,
-        context: &mut EvmContext<DB>,
-        inputs: &CallInputs,
-        outcome: CallOutcome,
-    ) -> CallOutcome {
+    fn call_end(&mut self, context: &mut EvmContext<DB>, inputs: &CallInputs, outcome: CallOutcome) -> CallOutcome {
         let outcome = self.gas_inspector.call_end(context, inputs, outcome);
-
         if context.journaled_state.depth() == 0 {
             self.print_summary(&outcome.result, context);
-            // clear the state if we are at the top level
             self.clear();
         }
-
         outcome
     }
 
-    fn create_end(
-        &mut self,
-        context: &mut EvmContext<DB>,
-        inputs: &CreateInputs,
-        outcome: CreateOutcome,
-    ) -> CreateOutcome {
+    fn create_end(&mut self, context: &mut EvmContext<DB>, inputs: &CreateInputs, outcome: CreateOutcome) -> CreateOutcome {
         let outcome = self.gas_inspector.create_end(context, inputs, outcome);
-
         if context.journaled_state.depth() == 0 {
             self.print_summary(&outcome.result, context);
-
-            // clear the state if we are at the top level
             self.clear();
         }
-
         outcome
     }
 }
@@ -276,11 +191,6 @@ fn hex_number(uint: u64) -> String {
 }
 
 fn hex_number_u256(b: &U256) -> String {
-    let s = hex::encode(b.to_be_bytes::<32>());
-    let s = s.trim_start_matches('0');
-    if s.is_empty() {
-        "0x0".to_string()
-    } else {
-        format!("0x{s}")
-    }
+    let s = hex::encode(b.to_be_bytes::<32>()).trim_start_matches('0').to_string();
+    if s.is_empty() { "0x0".to_string() } else { format!("0x{s}") }
 }
