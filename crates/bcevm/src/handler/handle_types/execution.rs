c3778e09ec77e4c9d702658c4f056d92f1943d2d
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-17 23:01:18 +0300

    Update execution.rs

--- crates/bcevm/src/handler/handle_types/execution.rs
+++ crates/bcevm/src/handler/handle_types/execution.rs
@@ -11,13 +11,11 @@ use bcevm_interpreter::{
     CallOutcome, CreateOutcome, EOFCreateInput, EOFCreateOutcome, InterpreterResult,
 };
 
-/// Handles first frame return handle.
 pub type LastFrameReturnHandle<'a, EXT, DB> = Arc<
     dyn Fn(&mut Context<EXT, DB>, &mut FrameResult) -> Result<(), EVMError<<DB as Database>::Error>>
         + 'a,
 >;
 
-/// Handle sub call.
 pub type FrameCallHandle<'a, EXT, DB> = Arc<
     dyn Fn(
             &mut Context<EXT, DB>,
@@ -26,7 +24,6 @@ pub type FrameCallHandle<'a, EXT, DB> = Arc<
         + 'a,
 >;
 
-/// Handle call return
 pub type FrameCallReturnHandle<'a, EXT, DB> = Arc<
     dyn Fn(
             &mut Context<EXT, DB>,
@@ -36,7 +33,6 @@ pub type FrameCallReturnHandle<'a, EXT, DB> = Arc<
         + 'a,
 >;
 
-/// Insert call outcome to the parent
 pub type InsertCallOutcomeHandle<'a, EXT, DB> = Arc<
     dyn Fn(
             &mut Context<EXT, DB>,
@@ -47,7 +43,6 @@ pub type InsertCallOutcomeHandle<'a, EXT, DB> = Arc<
         + 'a,
 >;
 
-/// Handle sub create.
 pub type FrameCreateHandle<'a, EXT, DB> = Arc<
     dyn Fn(
             &mut Context<EXT, DB>,
@@ -56,7 +51,6 @@ pub type FrameCreateHandle<'a, EXT, DB> = Arc<
         + 'a,
 >;
 
-/// Handle create return
 pub type FrameCreateReturnHandle<'a, EXT, DB> = Arc<
     dyn Fn(
             &mut Context<EXT, DB>,
@@ -66,7 +60,6 @@ pub type FrameCreateReturnHandle<'a, EXT, DB> = Arc<
         + 'a,
 >;
 
-/// Insert call outcome to the parent
 pub type InsertCreateOutcomeHandle<'a, EXT, DB> = Arc<
     dyn Fn(
             &mut Context<EXT, DB>,
@@ -76,7 +69,6 @@ pub type InsertCreateOutcomeHandle<'a, EXT, DB> = Arc<
         + 'a,
 >;
 
-/// Handle EOF sub create.
 pub type FrameEOFCreateHandle<'a, EXT, DB> = Arc<
     dyn Fn(
             &mut Context<EXT, DB>,
@@ -85,7 +77,6 @@ pub type FrameEOFCreateHandle<'a, EXT, DB> = Arc<
         + 'a,
 >;
 
-/// Handle EOF create return
 pub type FrameEOFCreateReturnHandle<'a, EXT, DB> = Arc<
     dyn Fn(
             &mut Context<EXT, DB>,
@@ -95,7 +86,6 @@ pub type FrameEOFCreateReturnHandle<'a, EXT, DB> = Arc<
         + 'a,
 >;
 
-/// Insert EOF crate outcome to the parent
 pub type InsertEOFCreateOutcomeHandle<'a, EXT, DB> = Arc<
     dyn Fn(
             &mut Context<EXT, DB>,
@@ -105,33 +95,20 @@ pub type InsertEOFCreateOutcomeHandle<'a, EXT, DB> = Arc<
         + 'a,
 >;
 
-/// Handles related to stack frames.
 pub struct ExecutionHandler<'a, EXT, DB: Database> {
-    /// Handles last frame return, modified gas for refund and
-    /// sets tx gas limit.
     pub last_frame_return: LastFrameReturnHandle<'a, EXT, DB>,
-    /// Frame call
     pub call: FrameCallHandle<'a, EXT, DB>,
-    /// Call return
     pub call_return: FrameCallReturnHandle<'a, EXT, DB>,
-    /// Insert call outcome
     pub insert_call_outcome: InsertCallOutcomeHandle<'a, EXT, DB>,
-    /// Frame crate
     pub create: FrameCreateHandle<'a, EXT, DB>,
-    /// Crate return
     pub create_return: FrameCreateReturnHandle<'a, EXT, DB>,
-    /// Insert create outcome.
     pub insert_create_outcome: InsertCreateOutcomeHandle<'a, EXT, DB>,
-    /// Frame EOFCreate
     pub eofcreate: FrameEOFCreateHandle<'a, EXT, DB>,
-    /// EOFCreate return
     pub eofcreate_return: FrameEOFCreateReturnHandle<'a, EXT, DB>,
-    /// Insert EOFCreate outcome.
     pub insert_eofcreate_outcome: InsertEOFCreateOutcomeHandle<'a, EXT, DB>,
 }
 
 impl<'a, EXT: 'a, DB: Database + 'a> ExecutionHandler<'a, EXT, DB> {
-    /// Creates mainnet ExecutionHandler.
     pub fn new<SPEC: Spec + 'a>() -> Self {
         Self {
             last_frame_return: Arc::new(mainnet::last_frame_return::<SPEC, EXT, DB>),
@@ -149,7 +126,6 @@ impl<'a, EXT: 'a, DB: Database + 'a> ExecutionHandler<'a, EXT, DB> {
 }
 
 impl<'a, EXT, DB: Database> ExecutionHandler<'a, EXT, DB> {
-    /// Handle call return, depending on instruction result gas will be reimbursed or not.
     #[inline]
     pub fn last_frame_return(
         &self,
@@ -159,7 +135,6 @@ impl<'a, EXT, DB: Database> ExecutionHandler<'a, EXT, DB> {
         (self.last_frame_return)(context, frame_result)
     }
 
-    /// Call frame call handler.
     #[inline]
     pub fn call(
         &self,
@@ -169,7 +144,6 @@ impl<'a, EXT, DB: Database> ExecutionHandler<'a, EXT, DB> {
         (self.call)(context, inputs.clone())
     }
 
-    /// Call registered handler for call return.
     #[inline]
     pub fn call_return(
         &self,
@@ -180,7 +154,6 @@ impl<'a, EXT, DB: Database> ExecutionHandler<'a, EXT, DB> {
         (self.call_return)(context, frame, interpreter_result)
     }
 
-    /// Call registered handler for inserting call outcome.
     #[inline]
     pub fn insert_call_outcome(
         &self,
@@ -192,7 +165,6 @@ impl<'a, EXT, DB: Database> ExecutionHandler<'a, EXT, DB> {
         (self.insert_call_outcome)(context, frame, shared_memory, outcome)
     }
 
-    /// Call Create frame
     #[inline]
     pub fn create(
         &self,
@@ -202,7 +174,6 @@ impl<'a, EXT, DB: Database> ExecutionHandler<'a, EXT, DB> {
         (self.create)(context, inputs)
     }
 
-    /// Call handler for create return.
     #[inline]
     pub fn create_return(
         &self,
@@ -213,7 +184,6 @@ impl<'a, EXT, DB: Database> ExecutionHandler<'a, EXT, DB> {
         (self.create_return)(context, frame, interpreter_result)
     }
 
-    /// Call handler for inserting create outcome.
     #[inline]
     pub fn insert_create_outcome(
         &self,
@@ -224,7 +194,6 @@ impl<'a, EXT, DB: Database> ExecutionHandler<'a, EXT, DB> {
         (self.insert_create_outcome)(context, frame, outcome)
     }
 
-    /// Call Create frame
     #[inline]
     pub fn eofcreate(
         &self,
@@ -234,7 +203,6 @@ impl<'a, EXT, DB: Database> ExecutionHandler<'a, EXT, DB> {
         (self.eofcreate)(context, inputs)
     }
 
-    /// Call handler for create return.
     #[inline]
     pub fn eofcreate_return(
         &self,
@@ -245,7 +213,6 @@ impl<'a, EXT, DB: Database> ExecutionHandler<'a, EXT, DB> {
         (self.eofcreate_return)(context, frame, interpreter_result)
     }
 
-    /// Call handler for inserting create outcome.
     #[inline]
     pub fn insert_eofcreate_outcome(
         &self,
