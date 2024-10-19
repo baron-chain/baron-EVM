7f1940775faeabcbb40543c99161d6981c98df16
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-19 23:44:23 +0300

    Update context_precompiles.rs

--- crates/bcevm/src/context/context_precompiles.rs
+++ crates/bcevm/src/context/context_precompiles.rs
@@ -5,149 +5,83 @@ use crate::{
 use core::ops::{Deref, DerefMut};
 use dyn_clone::DynClone;
 use bcevm_precompile::Precompiles;
-use std::{boxed::Box, sync::Arc};
+use std::{sync::Arc, boxed::Box};
 
 use super::InnebcevmContext;
 
-/// Precompile and its handlers.
 pub enum ContextPrecompile<DB: Database> {
-    /// Ordinary precompiles
     Ordinary(Precompile),
-    /// Stateful precompile that is Arc over [`ContextStatefulPrecompile`] trait.
-    /// It takes a reference to input, gas limit and Context.
-    ContextStateful(ContextStatefulPrecompileArc<DB>),
-    /// Mutable stateful precompile that is Box over [`ContextStatefulPrecompileMut`] trait.
-    /// It takes a reference to input, gas limit and context.
-    ContextStatefulMut(ContextStatefulPrecompileBox<DB>),
+    ContextStateful(Arc<dyn ContextStatefulPrecompile<DB>>),
+    ContextStatefulMut(Box<dyn ContextStatefulPrecompileMut<DB>>),
 }
 
 impl<DB: Database> Clone for ContextPrecompile<DB> {
     fn clone(&self) -> Self {
         match self {
-            Self::Ordinary(arg0) => Self::Ordinary(arg0.clone()),
-            Self::ContextStateful(arg0) => Self::ContextStateful(arg0.clone()),
-            Self::ContextStatefulMut(arg0) => Self::ContextStatefulMut(arg0.clone()),
+            Self::Ordinary(p) => Self::Ordinary(p.clone()),
+            Self::ContextStateful(p) => Self::ContextStateful(p.clone()),
+            Self::ContextStatefulMut(p) => Self::ContextStatefulMut(p.clone()),
         }
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Default)]
 pub struct ContextPrecompiles<DB: Database> {
     inner: HashMap<Address, ContextPrecompile<DB>>,
 }
 
 impl<DB: Database> ContextPrecompiles<DB> {
-    /// Returns precompiles addresses.
     #[inline]
     pub fn addresses(&self) -> impl Iterator<Item = &Address> {
         self.inner.keys()
     }
 
-    /// Extends the precompiles with the given precompiles.
-    ///
-    /// Other precompiles with overwrite existing precompiles.
     #[inline]
-    pub fn extend(
-        &mut self,
-        other: impl IntoIterator<Item = impl Into<(Address, ContextPrecompile<DB>)>>,
-    ) {
+    pub fn extend(&mut self, other: impl IntoIterator<Item = impl Into<(Address, ContextPrecompile<DB>)>>) {
         self.inner.extend(other.into_iter().map(Into::into));
     }
 
-    /// Call precompile and executes it. Returns the result of the precompile execution.
-    /// None if the precompile does not exist.
     #[inline]
-    pub fn call(
-        &mut self,
-        addess: Address,
-        bytes: &Bytes,
-        gas_price: u64,
-        evmctx: &mut InnebcevmContext<DB>,
-    ) -> Option<PrecompileResult> {
-        let precompile = self.inner.get_mut(&addess)?;
-
-        match precompile {
-            ContextPrecompile::Ordinary(p) => Some(p.call(bytes, gas_price, &evmctx.env)),
-            ContextPrecompile::ContextStatefulMut(p) => Some(p.call_mut(bytes, gas_price, evmctx)),
-            ContextPrecompile::ContextStateful(p) => Some(p.call(bytes, gas_price, evmctx)),
-        }
-    }
-}
-
-impl<DB: Database> Default for ContextPrecompiles<DB> {
-    fn default() -> Self {
-        Self {
-            inner: Default::default(),
-        }
+    pub fn call(&mut self, address: Address, bytes: &Bytes, gas_price: u64, evmctx: &mut InnebcevmContext<DB>) -> Option<PrecompileResult> {
+        self.inner.get_mut(&address).map(|precompile| match precompile {
+            ContextPrecompile::Ordinary(p) => p.call(bytes, gas_price, &evmctx.env),
+            ContextPrecompile::ContextStatefulMut(p) => p.call_mut(bytes, gas_price, evmctx),
+            ContextPrecompile::ContextStateful(p) => p.call(bytes, gas_price, evmctx),
+        })
     }
 }
 
 impl<DB: Database> Deref for ContextPrecompiles<DB> {
     type Target = HashMap<Address, ContextPrecompile<DB>>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.inner
-    }
+    fn deref(&self) -> &Self::Target { &self.inner }
 }
 
 impl<DB: Database> DerefMut for ContextPrecompiles<DB> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.inner
-    }
+    fn deref_mut(&mut self) -> &mut Self::Target { &mut self.inner }
 }
 
-/// Context aware stateful precompile trait. It is used to create
-/// a arc precompile in [`ContextPrecompile`].
 pub trait ContextStatefulPrecompile<DB: Database>: Sync + Send {
-    fn call(
-        &self,
-        bytes: &Bytes,
-        gas_price: u64,
-        evmctx: &mut InnebcevmContext<DB>,
-    ) -> PrecompileResult;
+    fn call(&self, bytes: &Bytes, gas_price: u64, evmctx: &mut InnebcevmContext<DB>) -> PrecompileResult;
 }
 
-/// Context aware mutable stateful precompile trait. It is used to create
-/// a boxed precompile in [`ContextPrecompile`].
 pub trait ContextStatefulPrecompileMut<DB: Database>: DynClone + Send + Sync {
-    fn call_mut(
-        &mut self,
-        bytes: &Bytes,
-        gas_price: u64,
-        evmctx: &mut InnebcevmContext<DB>,
-    ) -> PrecompileResult;
+    fn call_mut(&mut self, bytes: &Bytes, gas_price: u64, evmctx: &mut InnebcevmContext<DB>) -> PrecompileResult;
 }
 
 dyn_clone::clone_trait_object!(<DB> ContextStatefulPrecompileMut<DB>);
 
-/// Arc over context stateful precompile.
-pub type ContextStatefulPrecompileArc<DB> = Arc<dyn ContextStatefulPrecompile<DB>>;
-
-/// Box over context mutable stateful precompile
-pub type ContextStatefulPrecompileBox<DB> = Box<dyn ContextStatefulPrecompileMut<DB>>;
-
 impl<DB: Database> From<Precompile> for ContextPrecompile<DB> {
-    fn from(p: Precompile) -> Self {
-        ContextPrecompile::Ordinary(p)
-    }
+    fn from(p: Precompile) -> Self { ContextPrecompile::Ordinary(p) }
 }
 
 impl<DB: Database> From<Precompiles> for ContextPrecompiles<DB> {
     fn from(p: Precompiles) -> Self {
-        ContextPrecompiles {
-            inner: p.inner.into_iter().map(|(k, v)| (k, v.into())).collect(),
-        }
+        ContextPrecompiles { inner: p.inner.into_iter().map(|(k, v)| (k, v.into())).collect() }
     }
 }
 
 impl<DB: Database> From<&Precompiles> for ContextPrecompiles<DB> {
     fn from(p: &Precompiles) -> Self {
-        ContextPrecompiles {
-            inner: p
-                .inner
-                .iter()
-                .map(|(&k, v)| (k, v.clone().into()))
-                .collect(),
-        }
+        ContextPrecompiles { inner: p.inner.iter().map(|(&k, v)| (k, v.clone().into())).collect() }
     }
 }
