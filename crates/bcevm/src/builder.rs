accfbd72f4c613e8a54a9e2b6900fb42899a2795
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2025-03-01 23:59:47 +0200

    Update builder.rs

--- crates/bcevm/src/builder.rs
+++ crates/bcevm/src/builder.rs
@@ -1,4 +1,4 @@
-//BCMOD [ERR#0xc002e2] [ERR#0xc002e2] [ERR#0xc002e2] [ERR#0xc002e2] [ERR#0xc002e2] [ERR#0xc002e2] [ERR#0xc002e2] [ERR#0xc002e2] [ERR#0xc002e2] [ERR#0xc002e2]
+//BCMOD [ERR#0xc002e2] [ERR#0xc002e2] [ERR#0xc002e2] [ERR#0xc002e2] [ERR#0xc002e2] [ERR#0xc002e2] [ERR#0xc002e2] [ERR#0xc002e2] [ERR#0xc002e2] [ERR#0xc002e2] [ERR#0xc002e2]
 use crate::{
     db::{Database, DatabaseRef, EmptyDB, WrapDatabaseRef},
     handler::register,
